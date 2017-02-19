dc25e68066e5333e409b8c9ab64567b93a5d5004
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2017-02-19 03:59:13 -0600

    FDGKineticsWholebrain is working.

--- src/+mlsystem/TensorTools.m
+++ src/+mlsystem/TensorTools.m
@@ -9,45 +9,11 @@ classdef TensorTools
  	%% It was developed on Matlab 9.1.0.441655 (R2016b) for MACI64. 	
 
 	
+    properties
+        resizable
+    end
+    
 	methods (Static)
-        function [times,f] = shiftTensor(times, f, Dt)
-            import mlsystem.*
-            if (Dt > 0)
-                [times,f] = TensorTools.shiftTensorRight(times, f, Dt);
-                return
-            end
-            if (Dt < 0)
-                [times,f] = TensorTools.shiftTensorLeft( times, f, Dt);
-                return
-            end
-        end
-        function [times,f] = shiftTensorLeft(times0, f0, Dt)
-            %  Dt in sec
-            Dt    = abs(Dt);            
-            dt0   = abs(times0(2) - times0(1));
-            lenDt = floor(Dt/dt0);
-            len0  = length(times0);
-            len1  = len0 - lenDt;
-            
-            times = times0(1:len1);
-            assert(4 == length(size(f0)));
-            f     = f0(:,:,:,lenDt+1:end);
-        end
-        function [times,f] = shiftTensorRight(times0, f0, Dt)
-            %  Dt in sec
-            Dt    = abs(Dt);
-            dt0   = abs(times0(2) - times0(1));
-            lenDt = floor(Dt/dt0);
-            len0  = length(times0);
-            len1  = lenDt + len0;
-            
-            times1 = times0(end)+dt0:dt0:times0(end)+dt0*lenDt;
-            times  = [times0 times1];  
-            szf0   = size(f0);
-            assert(4 == length(szf0)); 
-            f      = f0(:,:,:,1).*ones(szf0(1),szf0(2),szf0(3),len1);
-            f(:,:,:,end-len0+1:end) = f0;
-        end
         function f = shiftTime(f, Dt, varargin)
             %% SHIFTTIME shifts the time-indices of vector f(t) or the right-most indices of tensor F(a, b, c, t).
             %  Usage:  F = TensorTools.shiftTime(F, Delta_t, property, property_value);
@@ -59,8 +25,70 @@ classdef TensorTools
             
             import mlsystem.*;
             f = TensorTools.shiftTensorTime(f, Dt, varargin{:});
-        end        
-    end 
+        end
+    end
+    
+    methods
+        function this = TensorTools(varargin)
+            ip = inputParser;
+            addOptional(ip, 'resizable', true, @islogical);
+            parse(ip, varargin{:});
+            this.resizable = ip.Results.resizable;
+        end
+        
+        function [times,f] = shiftTensor(this, times, f, Dt)
+            if (Dt > 0)
+                [times,f] = this.shiftTensorRight(times, f, Dt);
+                return
+            end
+            if (Dt < 0)
+                [times,f] = this.shiftTensorLeft( times, f, Dt);
+                return
+            end
+        end
+        function [times,f] = shiftTensorLeft(this, times0, f0, Dt)
+            %  Dt in sec
+            assert(4 == length(size(f0)));
+                
+            Dt    = abs(Dt);            
+            dt0   = abs(times0(2) - times0(1));
+            lenDt = floor(Dt/dt0);
+            len0  = length(times0);
+            len1  = len0 - lenDt;
+            
+            if (this.resizable)
+                times = times0(1:len1);
+                f     = f0(:,:,:,lenDt+1:end);
+                return
+            end            
+            times                = times0;
+            f                    = f0(:,:,:,end).*ones(size(f0));
+            f(:,:,:,1:end-lenDt) = f0(:,:,:,lenDt+1:end);
+        end
+        function [times,f] = shiftTensorRight(this, times0, f0, Dt)
+            %  Dt in sec
+            Dt    = abs(Dt);
+            dt0   = abs(times0(2) - times0(1));
+            lenDt = floor(Dt/dt0);
+            len0  = length(times0);
+            len1  = lenDt + len0;
+            
+            if (this.resizable)
+                times1 = times0(end)+dt0:dt0:times0(end)+dt0*lenDt;
+                times  = [times0 times1];  
+                szf0   = size(f0);
+                assert(4 == length(szf0)); 
+                f      = f0(:,:,:,1).*ones(szf0(1),szf0(2),szf0(3),len1);
+                f(:,:,:,end-len0+1:end) = f0;
+                return
+            end            
+            times                = times0;
+            f                    = f0(:,:,:,1).*ones(size(f0));
+            f(:,:,:,lenDt+1:end) = f0(:,:,:,1:end-lenDt);
+        end
+    end
+    
+    %% PRIVATE
     
     methods (Static, Access = 'private')
         function f = shiftTensorTime(f, Dt, varargin)
@@ -83,6 +111,7 @@ classdef TensorTools
             end                
         end
     end
+    
 	%  Created with Newcl by John J. Lee after newfcn by Frank Gonzalez-Morphy
  end
 
--- src/+mlsystem/VectorTools.m
+++ src/+mlsystem/VectorTools.m
@@ -8,49 +8,12 @@ classdef VectorTools
  	%  and checked into repository $URL$,  
  	%  developed on Matlab 8.4.0.150421 (R2014b) 
  	%  $Id$ 
+    
+    properties
+        resizable
+    end
  	 
 	methods (Static)
-        function [times,f] = shiftVector(times, f, Dt)
-            %% SHIFTVECTOR shifts f backwards in time by Delta t < 0 and forwards in time by Delta t > 0.
-            %  The data window contracts for backward shifts, losing data, and expands for forward shifts 
-            %  without loss of data.  Internal operations use row vectors.   
-            
-            import mlsystem.*
-            times = VectorTools.ensureRowVector(times);
-            f     = VectorTools.ensureRowVector(f);
-            if (Dt > 0)
-                [times,f] = VectorTools.shiftVectorRight(times, f, Dt);
-                return
-            end
-            if (Dt < 0)
-                [times,f] = VectorTools.shiftVectorLeft( times, f, Dt);
-                return
-            end
-        end
-        function [times,f] = shiftVectorLeft(times0, f0, Dt)
-            %  Dt in sec
-            Dt    = abs(Dt);            
-            dt0   = abs(times0(2) - times0(1));
-            lenDt = floor(Dt/dt0);
-            len0  = length(times0);
-            len1  = len0 - lenDt;
-            
-            times = times0(1:len1);
-            f     = f0(lenDt+1:end);
-        end
-        function [times,f] = shiftVectorRight(times0, f0, Dt)
-            %  Dt in sec
-            Dt    = abs(Dt);
-            dt0   = abs(times0(2) - times0(1));
-            lenDt = floor(Dt/dt0);
-            len0  = length(times0);
-            len1  = lenDt + len0;
-            
-            times1 = times0(end)+dt0:dt0:times0(end)+dt0*lenDt;
-            times  = [times0 times1];   
-            f      = f0(1) * ones(1,len1);            
-            f(end-len0+1:end) = f0;
-        end 
         function x = ensureColVector(x)
             %% ENSURECOLVECTOR reshapes row vectors to col vectors, leaving matrices untouched
             
@@ -64,6 +27,69 @@ classdef VectorTools
             if (size(x,1) > size(x,2)); x = x'; end
         end       
     end 
+    
+    methods 
+        function this = VectorTools(varargin)
+            ip = inputParser;
+            addOptional(ip, 'resizable', true, @islogical);
+            parse(ip, varargin{:});
+            this.resizable = ip.Results.resizable;
+        end
+        
+        function [times,f] = shiftVector(this, times, f, Dt)
+            %% SHIFTVECTOR shifts f backwards in time by Delta t < 0 and forwards in time by Delta t > 0.
+            %  If VectorTools.resizable, the data window contracts for backward shifts, losing data, and  
+            %  expands for forward shifts without loss of data.  Internal operations use row vectors.   
+            
+            import mlsystem.*
+            times = VectorTools.ensureRowVector(times);
+            f     = VectorTools.ensureRowVector(f);
+            if (Dt > 0)
+                [times,f] = this.shiftVectorRight(times, f, Dt);
+                return
+            end
+            if (Dt < 0)
+                [times,f] = this.shiftVectorLeft( times, f, Dt);
+                return
+            end
+        end
+        function [times,f] = shiftVectorLeft(this, times0, f0, Dt)
+            %  Dt in sec
+            Dt    = abs(Dt);            
+            dt0   = abs(times0(2) - times0(1));
+            lenDt = floor(Dt/dt0);
+            len0  = length(times0);
+            len1  = len0 - lenDt;
+            
+            if (this.resizable)
+                times = times0(1:len1);
+                f     = f0(lenDt+1:end);
+                return
+            end
+            times          = times0;
+            f              = f0(end)*ones(size(f0));
+            f(1:end-lenDt) = f0(lenDt+1:end);
+        end
+        function [times,f] = shiftVectorRight(this, times0, f0, Dt)
+            %  Dt in sec
+            Dt    = abs(Dt);
+            dt0   = abs(times0(2) - times0(1));
+            lenDt = floor(Dt/dt0);
+            len0  = length(times0);
+            len1  = lenDt + len0;            
+            
+            if (this.resizable)
+                times1 = times0(end)+dt0:dt0:times0(end)+dt0*lenDt;
+                times  = [times0 times1];   
+                f      = f0(1) * ones(1,len1);            
+                f(end-len0+1:end) = f0;
+                return
+            end
+            times          = times0;
+            f              = f0(1)*ones(size(f0));
+            f(lenDt+1:end) = f0(1:end-lenDt);
+        end 
+    end
 
 	%  Created with Newcl by John J. Lee after newfcn by Frank Gonzalez-Morphy 
 end
