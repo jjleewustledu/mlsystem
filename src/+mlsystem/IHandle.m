a99884c2ddd0e115b7549edac1c7a28e4f931198
John Lee <16329959+jjleewustledu@users.noreply.github.com>
2023-10-31 14:13:55 -0500

    tidying

---
+++ src/+mlsystem/IHandle.m
@@ -0,0 +1,8 @@
+classdef (Abstract) IHandle < handle & matlab.mixin.Heterogeneous & matlab.mixin.Copyable
+    %% provides preferred handle functionality
+    %  
+    %  Created 02-May-2023 14:49:02 by jjlee in repository /Users/jjlee/MATLAB-Drive/mlkinetics/src/+mlkinetics.
+    %  Developed on Matlab 9.14.0.2239454 (R2023a) Update 1 for MACI64.  Copyright 2023 John J. Lee.
+    
+    %  Created with mlsystem.Newcl, inspired by Frank Gonzalez-Morphy's newfcn.
+end
